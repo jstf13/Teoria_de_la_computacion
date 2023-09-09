742f62d00824b54fb34e41ec4b96e698a3da205e
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-09-09 00:04:12 -0300

    Saving Tarea0 finished

--- Tarea0/Tarea0.hs
+++ Tarea0/Tarea0.hs
@@ -68,97 +68,68 @@ included (x:xs) ls
     | belongs x ls == True = included xs ls
     | belongs x ls == False = False
 
--- Evaluación de expresiones
--- eval :: Memory -> Exp -> (Memory, Value)
--- eval mem (Var x) = case lkup x mem of
---     Just v -> (mem, v)
---     Nothing -> error "Variable no encontrada en la memoria"
--- eval mem Empty = (mem, Set [])
--- eval mem (Singleton n) = (mem, Set [n])
--- eval mem (Member e1 e2) =
---     let (mem', v1) = eval mem e1
---         (mem'', v2) = eval mem' e2
---     in case (v1, v2) of
---         (Set c1, Set c2) -> (mem'', BoolValue (belongs (head c1) c2))
---         _ -> error "Tipos incorrectos en Member"
--- eval mem (Union e1 e2) =
---     let (mem', v1) = eval mem e1
---         (mem'', v2) = eval mem' e2
---     in case (v1, v2) of
---         (Set c1, Set c2) -> (mem'', Set (union c1 c2))
---         _ -> error "Tipos incorrectos en Union"
--- eval mem (Intersection e1 e2) =
---     let (mem', v1) = eval mem e1
---         (mem'', v2) = eval mem' e2
---     in case (v1, v2) of
---         (Set c1, Set c2) -> (mem'', Set (intersection c1 c2))
---         _ -> error "Tipos incorrectos en Intersection"
--- eval mem (Difference e1 e2) =
---     let (mem', v1) = eval mem e1
---         (mem'', v2) = eval mem' e2
---     in case (v1, v2) of
---         (Set c1, Set c2) -> (mem'', Set (difference c1 c2))
---         _ -> error "Tipos incorrectos en Difference"
--- eval mem (Subset e1 e2) =
---     let (mem', v1) = eval mem e1
---         (mem'', v2) = eval mem' e2
---     in case (v1, v2) of
---         (Set c1, Set c2) -> (mem'', BoolValue (included c1 c2))
---         _ -> error "Tipos incorrectos en Subset"
--- eval mem (Assign x e) =
---     let (mem', v) = eval mem e
---     in (upd mem' (x, v), v)
-
-
--- -- Expresiones de conjuntos
--- conj1 :: Exp
--- conj1 = Union (Union (Singleton 1) (Singleton 2)) (Singleton 3)
+eval :: Memory -> Exp -> (Memory, Value)
+eval mem Empty = (mem, Set [])
+eval mem (Var x) = case lkup x mem of
+    Set c -> (mem, Set c)
+    BoolValue b -> (mem, BoolValue b)
+eval mem (Singleton x) = (mem, Set [x])
+eval mem (Member e1 e2) = case (eval mem e1, eval mem e2) of
+    ((_, Set c1), (_, Set c2)) -> 
+        (mem, BoolValue (any (\x -> belongs x c1) c2))
+    _ -> error "Tipos incorrectos en Member"
+eval mem (Union e1 e2) = case (eval mem e1, eval mem e2) of
+    ((_, Set c1), (_, Set c2)) -> (mem, Set (union c1 c2))
+eval mem (Intersection e1 e2) = case (eval mem e1, eval mem e2) of
+    ((_, Set c1), (_, Set c2)) -> (mem, Set (intersection c1 c2))
+eval mem (Difference e1 e2) = case (eval mem e1, eval mem e2) of
+    ((_, Set c1), (_, Set c2)) -> (mem, Set (difference c1 c2))
+eval mem (Subset e1 e2) = case (eval mem e1, eval mem e2) of
+    ((_, Set c1), (_, Set c2)) -> (mem, BoolValue (included c1 c2))
+eval mem (Assign x e1) = case (eval mem e1) of
+    (m1, Set c1) -> (upd m1 (x, Set c1), Set c1)
+    
 
 conj1 :: Exp
 conj1 = Singleton 1 `Union` Singleton 2 `Union` Singleton 3
 
--- conj2 :: Exp
--- conj2 = Singleton 2 `Union` Singleton 3 `Union` Singleton 4
+conj2 :: Exp
+conj2 = Union (Singleton 2) (Union (Singleton 3) (Singleton 4))
 
--- conj3 :: Exp
--- conj3 = conj1 `Union` conj2
+conj3 :: Exp
+conj3 = conj1 `Union` conj2
 
--- conj4 :: Exp
--- conj4 = conj1 `Intersection` conj2
+conj4 :: Exp
+conj4 = Intersection conj1 conj2
 
--- pert1 :: Exp
--- pert1 = Member (Singleton 2) conj1
+pert1 :: Exp
+pert1 = Member (Singleton 2) conj1
 
--- pert2 :: Exp
--- pert2 = Member (Singleton 3) conj4
+pert2 :: Exp
+pert2 = Member (Singleton 3) conj4
 
--- incl1 :: Exp
--- incl1 = Subset conj1 conj2
+incl1 :: Exp
+incl1 = Subset conj1 conj2
 
--- incl2 :: Exp
--- incl2 = Subset conj4 (Singleton 2 `Union` Singleton 3 `Union` Singleton 4)
+incl2 :: Exp
+incl2 = Subset conj4 conj2
 
--- incl3 :: Exp
--- incl3 = Subset conj1 (conj1 `Union` conj2)
+incl3 :: Exp
+incl3 = Subset conj1 (conj1 `Union` conj2)
 
--- ass1 :: Exp
--- ass1 = Assign "w" conj1
+ass1 :: Exp
+ass1 = Assign "w" conj1
 
--- ass2 :: Exp
--- ass2 = Assign "x" conj4
+ass2 :: Exp
+ass2 = Assign "x" conj4
 
--- ass3 :: Exp
--- ass3 = Assign "y" pert2
+ass3 :: Exp
+ass3 = Assign "y" pert2
 
--- ass4 :: Exp
--- ass4 = Assign "z" incl2
+ass4 :: Exp
+ass4 = Assign "z" incl2
 
--- Ejemplo de uso
--- main :: IO ()
--- main = do
---     let initialMem = []
---     let (finalMem, result) = eval initialMem conj1
---     putStrLn $ "Resultado: " ++ show result
+
 
 
 -- Ejemplo de una memoria con variables definidas
@@ -166,5 +137,15 @@ sampleMemory :: Memory
 sampleMemory = [
     ("x", Set [1, 2, 3]),          -- Asignación de un conjunto
     ("y", Set [2, 3, 4]),          -- Asignación de otro conjunto
-    ("z", BoolValue True)         -- Asignación de un valor booleano
+    ("z", BoolValue True)          -- Asignación de un valor booleano
     ]
+
+-- Ejemplo de definicion de variables
+var1 :: Exp
+var1 = Var "x"
+
+var2 :: Exp
+var2 = Var "y"
+
+var3 :: Exp
+var3 = Var "j"
